f367a291bc6dc72d862072b6b960db619c38ce80
cshahani <chirag.shahani@alcatel-lucent.com>
2016-01-06 14:17:23 -0800

    Port of bug os-1110 to stable/liberty
    Change-Id: Ib01a4ea2e5fbfb0c330b124389a94419e5ba4d45

--- nuage_neutron/plugins/nuage/plugin.py
+++ nuage_neutron/plugins/nuage/plugin.py
@@ -239,9 +239,12 @@ class NuagePlugin(base_plugin.BaseNuagePlugin,
             'netpart_name': np_name,
             'ip': port['fixed_ips'][0]['ip_address'],
             'no_of_ports': no_of_ports,
-            'tenant': subn['tenant_id'],
+            'tenant': port['tenant_id'],
+            'netpart_id': subnet_mapping['net_partition_id'],
             'neutron_id': port['fixed_ips'][0]['subnet_id'],
-            'vport_id': nuage_port.get('nuage_vport_id')
+            'vport_id': nuage_port.get('nuage_vport_id'),
+            'subn_tenant': subn['tenant_id'],
+            'portOnSharedSubn': subn['shared']
         }
 
         if subnet_mapping['nuage_managed_subnet']:
@@ -434,7 +437,7 @@ class NuagePlugin(base_plugin.BaseNuagePlugin,
                       self)._delete_port_security_group_bindings(context,
                                                                  port_id)
         # Convert to list as a set might be passed here and
-        # this has to be serialized
+        # this has to be serialized.
         port[ext_sg.SECURITYGROUPS] = (list(sec_group) if sec_group else [])
 
     @log_helpers.log_method_call
@@ -1025,10 +1028,14 @@ class NuagePlugin(base_plugin.BaseNuagePlugin,
             params = {
                 'no_of_ports': no_of_ports,
                 'netpart_name': np_name,
-                'tenant': subn['tenant_id'],
                 'mac': port['mac_address'],
+                'tenant': port['tenant_id'],
                 'nuage_vif_id': nuage_vif_id,
-                'id': vm_id
+                'id': vm_id,
+                'subn_tenant': subn['tenant_id'],
+                'l2dom_id': l2dom_id,
+                'l3dom_id': l3dom_id,
+                'portOnSharedSubn': subn['shared']
             }
             self.nuageclient.delete_vms(params)
 
