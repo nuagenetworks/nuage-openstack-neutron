1cf1b604230b67fe78128ada015cd1f750eba368
cshahani <chirag.shahani@alcatel-lucent.com>
2016-02-02 10:54:39 -0800

    Fix for os-1195
    Added support for user to set backhaul rd and backhaul rt using
    router-create cli.
    Added support for user to update backhaul rd and backhaul rt using
    router-update cli
    
    Change-Id: Iea097568bc81430325365d4ec3dd6fc4c74b2c19
    Closes-Bug: Openstack-1195

--- nuage_neutron/plugins/nuage/plugin.py
+++ nuage_neutron/plugins/nuage/plugin.py
@@ -2186,6 +2186,10 @@ class NuagePlugin(base_plugin.BaseNuagePlugin,
         router['rt'] = nuage_router.get('routeTarget')
         router['ecmp_count'] = nuage_router.get('ECMPCount')
         router['nuage_backhaul_vnid'] = nuage_router.get('backHaulVNID')
+        router['nuage_backhaul_rd'] = (nuage_router.get(
+            'backHaulRouteDistinguisher'))
+        router['nuage_backhaul_rt'] = nuage_router.get('backHaulRouteTarget')
+
         for route in router.get('routes', []):
             params = {
                 'address': route['destination'].split("/")[0],
@@ -2243,6 +2247,10 @@ class NuagePlugin(base_plugin.BaseNuagePlugin,
             neutron_router['ecmp_count'] = nuage_router['ecmp_count']
             neutron_router['nuage_backhaul_vnid'] = \
                 nuage_router['nuage_backhaul_vnid']
+            neutron_router['nuage_backhaul_rd'] = \
+                nuage_router['nuage_backhaul_rd']
+            neutron_router['nuage_backhaul_rt'] = \
+                nuage_router['nuage_backhaul_rt']
 
         return neutron_router
 
