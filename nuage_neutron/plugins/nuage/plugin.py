f42f876b67764aa4fad209ef8885f62b3d98955a
Wim De Clercq <Wim.De_Clercq@alcatel-lucent.com>
2016-11-15 16:31:03 +0100

    Bring is_attr_set back for a port's security groups.
    In the average use case, when the user does not specify a SG for the port,
    the default security group will be filled in, so this is not needed.
    However, in case a user sets portsecurity to False. This behaviour does not
    happen and the security_group ID list will still be the
    'ATTR_NOT_SPECIFIED' object.
    
    Change-Id: I60452cb8db59bd603b372b78b3dd4cfd869e54d5
    Closes-Bug: OPENSTACK-1610

--- nuage_neutron/plugins/nuage/plugin.py
+++ nuage_neutron/plugins/nuage/plugin.py
@@ -334,6 +334,9 @@ class NuagePlugin(port_dhcp_options.PortDHCPOptionsNuage,
     @log.log
     def _process_port_create_security_group(self, context, port, vport, sg_ids,
                                             vsd_subnet):
+        if not attributes.is_attr_set(sg_ids):
+            port[ext_sg.SECURITYGROUPS] = []
+            return
         if len(sg_ids) > 6:
             msg = (_("Exceeds maximum num of security groups on a port "
                      "supported on nuage VSP"))
