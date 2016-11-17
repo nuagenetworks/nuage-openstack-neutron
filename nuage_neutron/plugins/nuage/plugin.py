105e86866265a1397abfe258458fc2e5d9918ba5
adityarn <aditya_reddy.nagaram@alcatel-lucent.com>
2016-11-17 14:00:44 +0100

    In fix for OPENSTACK-1615 the case of value being -1 was not taken in to consideration. Fixing that now
    Change-Id: I1aa615e45a92891fed7de024e1000dcc8c51d656

--- nuage_neutron/plugins/nuage/plugin.py
+++ nuage_neutron/plugins/nuage/plugin.py
@@ -2755,8 +2755,8 @@ class NuagePlugin(port_dhcp_options.PortDHCPOptionsNuage,
                         neutron_fip['nuage_egress_fip_rate_kbps'] = value
                 else:
                     rate_unit = 'M'
-                    neutron_fip['nuage_egress_fip_rate_kbps'] = float(value
-                                                                      ) * 1000
+                    neutron_fip['nuage_egress_fip_rate_kbps'] = float(
+                        value) * 1000 if float(value) != -1 else -1
                 self.fip_rate_log.info(
                     'FIP %s (owned by tenant %s) %s updated to %s %sb/s'
                     % (neutron_fip['id'], neutron_fip['tenant_id'],
@@ -3019,8 +3019,8 @@ class NuagePlugin(port_dhcp_options.PortDHCPOptionsNuage,
                         neutron_fip['nuage_egress_fip_rate_kbps'] = value
                 else:
                     rate_unit = 'M'
-                    neutron_fip['nuage_egress_fip_rate_kbps'] = float(value
-                                                                      ) * 1000
+                    neutron_fip['nuage_egress_fip_rate_kbps'] = float(
+                        value) * 1000 if float(value) != -1 else -1
                 self.fip_rate_log.info(
                     'FIP %s (owned by tenant %s) %s updated to %s %sb/s'
                     % (orig_fip['id'], orig_fip['tenant_id'], direction, value,
