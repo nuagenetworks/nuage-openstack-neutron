e0eeefcfc38d45964717054d442281b1d4e5a023
Wim De Clercq <Wim.De_Clercq@alcatel-lucent.com>
2016-11-28 13:20:38 +0100

    Fix DB issues seen in rally tests.
    - Have @neutron.db.api.retry_if_session_inactive on most of the basic crud
      methods if inside of these there is a DB action.
    - @handle_nuage_api_error only handles RESTProxyError
    - @handle_nuage_api_error removed on private plugin methods. Only remaining
      on the plugin's entry point methods (eg. create_x, update_x, delete_x,
      get_x)
    
    Change-Id: Ifcefcd2808c53acf9b14aaacd1cccfbff79a567f
    OPENSTACK-1627

--- nuage_neutron/plugins/nuage/gateway.py
+++ nuage_neutron/plugins/nuage/gateway.py
@@ -79,7 +79,6 @@ class NuagegatewayMixin(object):
             res['tenant_id'] = context.tenant_id
         return self._fields(res, fields)
 
-    @utils.handle_nuage_api_error
     @log_helpers.log_method_call
     def _make_vport_dict(self, vport, fields=None, context=None):
         res = {
@@ -272,7 +271,6 @@ class NuagegatewayMixin(object):
     def get_nuage_gateway_vports_count(self, context, filters=None):
         return 0
 
-    @utils.handle_nuage_api_error
     @log_helpers.log_method_call
     def _check_for_permissions(self, context, user_tenant):
         fetch_tenant = None
