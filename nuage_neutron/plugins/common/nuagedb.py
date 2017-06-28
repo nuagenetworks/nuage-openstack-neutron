d69f3296e57b6b4e30b1dd5d9fc7ef18d47e4477
tstappae <tom.stappaerts@nokia.com>
2017-06-28 15:41:04 +0200

    Allow Fip2VIP for device_owner_prefix ports
    Currently we are only treating device_owner_prefix ports as VIP
    at port creation. We will now treat device_owner_prefix as
    nuage:vip port for fip2vip scenarios.
    
    Closes-Bug: OPENSTACK-1837, OPENSTACK-1838
    Change-Id: I49e416d6979a72ca910464635dcd99d1373691d2

--- nuage_neutron/plugins/common/nuagedb.py
+++ nuage_neutron/plugins/common/nuagedb.py
@@ -21,9 +21,9 @@ from neutron.db.models import securitygroup as securitygroups_db
 from neutron.db import models_v2
 from neutron_lib import constants as os_constants
 
-from nuage_neutron.plugins.common import constants as nuage_constants
 from nuage_neutron.plugins.common import exceptions
 from nuage_neutron.plugins.common import nuage_models
+from nuage_neutron.plugins.common import utils
 
 
 def add_net_partition(session, netpart_id,
@@ -291,8 +291,7 @@ def get_floatingip_per_vip_in_network(session, network_id):
              l3_db.FloatingIP.fixed_port_id == models_v2.Port.id))
         .filter(
             models_v2.Port.network_id == network_id,
-            models_v2.Port.device_owner ==
-            nuage_constants.DEVICE_OWNER_VIP_NUAGE
+            models_v2.Port.device_owner.in_(utils.get_device_owners_vip())
         )
     ).all()
     fips_per_vip = {}
