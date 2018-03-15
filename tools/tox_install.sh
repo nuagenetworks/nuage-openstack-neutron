ef0e7f84c61fe7d83d12808dcf279059660cec17
Vlad Gridin <Vladyslav.Gridin@alcatel-lucent.com>
2018-03-15 08:31:10 +0100

    Move neutron to requirements.txt
    Since neutron is now in global-requirements, we can remove
    the installation from tools/tox_install.sh.
    
    Change-Id: I2edd847ae74933916c244335374a61626c32f537

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -5,25 +5,12 @@
 set -ex
 
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
-NEUTRON_BRANCH=${NEUTRON_BRANCH:-master}
 UPPER_CONSTRAINTS_FILE=${UPPER_CONSTRAINTS_FILE:-unconstrained}
 
 install_cmd="pip install"
 
 if [ "$UPPER_CONSTRAINTS_FILE" != "unconstrained" ]; then
     install_cmd="$install_cmd -c$UPPER_CONSTRAINTS_FILE"
-fi
-
-if $(python -c "import neutron" 2> /dev/null); then
-    echo "Neutron already installed."
-elif [ -x $ZUUL_CLONER ]; then
-    # Use zuul-cloner to clone openstack/neutron, this will ensure the Depends-On
-    # references are retrieved from zuul and rebased into the repo, then installed.
-    $ZUUL_CLONER --cache-dir /opt/git --branch $NEUTRON_BRANCH --workspace /tmp git://git.openstack.org openstack/neutron
-    $install_cmd /tmp/openstack/neutron
-else
-    # Install neutron from git.openstack.org
-    $install_cmd -e git+https://git.openstack.org/openstack/neutron@$NEUTRON_BRANCH#egg=neutron
 fi
 
 # Install the rest of the requirements as normal
