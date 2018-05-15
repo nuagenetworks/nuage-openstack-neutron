45741e7145422153d17138ef86226b8bc4f1693e
Vlad Gridin <Vladyslav.Gridin@alcatel-lucent.com>
2018-05-15 16:00:46 +0200

    deploy vrs and metadata agent from stack.sh
    Change-Id: I6fe476128a8d5e12338689e0bbcddbec124eb89b

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -22,13 +22,27 @@ source $DIR_NUAGE/devstack/functions
 if [[ "$1" == "stack" ]]; then
     source $DIR_NUAGE/devstack/lib/$Q_PLUGIN
     if [[ "$2" == "install" ]]; then
+        if [ "$NUAGE_CREATE_FAKE_UPLINK" == "True" ]; then
+            echo_summary "Creating fake uplink itf"
+            if ! ip addr show fake_interface; then
+                sudo ip link add fake_interface type dummy
+                sudo ip link set fake_interface up
+            fi
+            sudo ip addr add $PUBLIC_NETWORK_GATEWAY/$UPLINK_PREFIX_LEN dev fake_interface
+        fi
+        echo_summary "Configuring Nuage VRS"
+        configure_vrs_nuage
         echo_summary "Installing Nuage plugin"
         setup_develop $DIR_NUAGE
 
     elif [[ "$2" == "post-config" ]]; then
         mkdir -v -p $NEUTRON_CONF_DIR/policy.d && cp -v $DIR_NUAGE/etc/neutron/policy.d/nuage_policy.json $NEUTRON_CONF_DIR/policy.d
         configure_neutron_nuage
-
+        configure_nova_nuage
+        if [[ "${NUAGE_USE_METADATA}" == "True" ]]; then
+            # Tweak the chain for nuage metadata proxy.
+            sudo iptables -I openstack-INPUT 1 -i ${OVS_BRIDGE} -j ACCEPT || :
+        fi
     elif [[ "$2" == "test-config" ]]; then
         # must run after Octavia is running.
         configure_octavia_nuage
@@ -36,4 +50,7 @@ if [[ "$1" == "stack" ]]; then
 
 elif [[ "$1" == "unstack" ]]; then
         stop_octavia_nuage
+        if [ "$NUAGE_CREATE_FAKE_UPLINK" == "True" ]; then
+            sudo ip link delete fake_interface
+        fi
 fi
