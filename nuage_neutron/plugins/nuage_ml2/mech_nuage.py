b59eb392b7a96deb6b541a54d8933c18c7007bc6
marcelo <marcelo.guerrero_viveros@nokia.com>
2020-01-31 10:52:25 +0100

    Allow all ipv6 prefixes in neutron
    Change-Id: I5d3a0650ff57540e08c4bfad77e703997f26838d
    Closes-Bug: PROD-11034
    Depends-On: http://gerrit.an.nuagenetworks.net:8080/c/OpenStack/nuage-tempest-plugin/+/401

--- nuage_neutron/plugins/nuage_ml2/mech_nuage.py
+++ nuage_neutron/plugins/nuage_ml2/mech_nuage.py
@@ -311,10 +311,6 @@ class NuageMechanismDriver(base_plugin.RootNuagePlugin,
     def _validate_create_subnet(self, db_context,
                                 network, prefixlen, subnet, vsd_managed,
                                 l2bridge):
-        if self._is_ipv6(subnet) and prefixlen != 64:
-            msg = _('Invalid IPv6 prefix length. '
-                    'Only prefix length 64 is supported.')
-            raise NuageBadRequest(resource='subnet', msg=msg)
         for attribute in ('ipv6_ra_mode', 'ipv6_address_mode'):
             if not lib_validators.is_attr_set(subnet.get(attribute)):
                 continue
