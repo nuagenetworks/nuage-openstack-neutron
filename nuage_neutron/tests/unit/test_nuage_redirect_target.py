e3540e1efdca989bed6a95aa9b2df830a3fc1f7f
Tom Stappaerts <tom.stappaerts@nokia.com>
2020-04-24 16:14:18 +0200

    Concurrent create of redirect target NoneType error
    When creating a redirect target concurrently, we do not
    create the Policygroup rules multiple times, however this
    duplicate rules prevention results in a NoneType error, as
    we do not return the actual PolicyGroup that was concurrently
    created.
    Added a lookup for the concurrently created PG.
    Added a unittest for this scenario.
    
    Change-Id: Id4996cba2cb0d6abda4fdc4f2825df3c2035d1de
    Closes-Bug: OPENSTACK-2865

--- nuage_neutron/tests/unit/test_nuage_redirect_target.py
+++ nuage_neutron/tests/unit/test_nuage_redirect_target.py
@@ -16,27 +16,31 @@ import mock
 import testtools
 
 from nuage_neutron.plugins.common.base_plugin import RootNuagePlugin
-from nuage_neutron.plugins.nuage_ml2 import securitygroup
+from nuage_neutron.plugins.common.service_plugins.port_attributes \
+    import nuage_redirect_target
 from nuage_neutron.vsdclient import restproxy
 
 
-class TestNuageSecurityGroup(testtools.TestCase):
+class TestNuageRedirectTarget(testtools.TestCase):
 
     @mock.patch.object(RootNuagePlugin, 'init_vsd_client')
-    @mock.patch.object(securitygroup.NuageSecurityGroup, 'core_plugin')
-    def test_create_policygroup(self, *_):
-        driver = securitygroup.NuageSecurityGroup()
+    @mock.patch.object(nuage_redirect_target.NuageRedirectTarget,
+                       'core_plugin')
+    def test_create_pg_for_rt(self, *_):
+        driver = nuage_redirect_target.NuageRedirectTarget()
         fake_sg = {
             'security_group_rules': []
         }
         driver.core_plugin.get_security_group = mock.MagicMock(
             return_value=fake_sg)
-        vsd_mock = mock.MagicMock()
-        driver.vsdclient = vsd_mock
-        vsd_mock.create_security_group.side_effect = (
+        vsdclient_mock = mock.MagicMock()
+        driver.vsdclient = vsdclient_mock
+        vsdclient_mock.create_security_group_using_parent.side_effect = (
             restproxy.RESTProxyError(
                 vsd_code=restproxy.REST_PG_EXISTS_ERR_CODE))
-        pg = driver._create_policygroup(mock.MagicMock(), mock.MagicMock(),
-                                        mock.MagicMock())
+        rt = {'parentID': '1', 'parentType': 'l2domain'}
+        pg = driver._create_pg_for_rt(mock.MagicMock(), mock.MagicMock(),
+                                      rt, mock.MagicMock())
         self.assertIsNotNone(pg)
-        vsd_mock.create_security_group_rules.assert_not_called()
+        vsdclient_mock.create_security_group_rules.assert_not_called()
+        vsdclient_mock.get_nuage_l2domain_policy_groups.assert_called()
