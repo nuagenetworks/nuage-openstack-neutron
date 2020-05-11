8a26235b49856962367532e64885acb633d5d611
Lina He <lina.he@nokia.com>
2020-05-11 05:23:36 +0200

    Fix duplicate priorities of redirect target rule
    Redirect target rule failed to recreate due to the existent entry
    on VSD. VSD returns a duplicate priority error. To handle
    this error, we get the created one from vsd.
    
    Closes-Bug: OPENSTACK-2880
    Change-Id: Ie77932ba715d5220493a3f9db40da66418cdd972

--- nuage_neutron/vsdclient/resources/policygroups.py
+++ nuage_neutron/vsdclient/resources/policygroups.py
@@ -887,7 +887,7 @@ class NuagePolicyGroups(object):
             rest_data = nuageacl.post_data_default_l3()
         return self.restproxy.post(
             rest_path, rest_data,
-            on_res_exists=self.restproxy.acltmpl_retrieve_by_priority,
+            on_res_exists=self.restproxy.retrieve_by_cms_id_and_priority,
             ignore_err_codes=[restproxy.REST_DUPLICATE_ACL_PRIORITY])[0]
 
     def create_default_deny_rule(self, parent_id, parent_type,
@@ -1528,7 +1528,10 @@ class NuageRedirectTargets(object):
         # neutron ingress is nuage egress and vice versa
         fwd_rules = self.restproxy.post(
             nuage_fwdrule.in_post_resource(fwd_policy_id),
-            nuage_match_info)
+            nuage_match_info,
+            on_res_exists=self.restproxy.retrieve_by_ext_id_and_priority,
+            ignore_err_codes=[restproxy.REST_DUPLICATE_POLICY_ENTRY_PRIORITY]
+        )
         return (self._process_redirect_target_rule(fwd_rules[0])
                 if fwd_rules else None)
 
