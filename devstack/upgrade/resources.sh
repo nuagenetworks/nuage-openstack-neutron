72a0e9aa48e38be56195e107fedacbdf7bb27a6c
Vlad Gridin <vladyslav.gridin@nokia.com>
2020-09-01 10:17:25 +0200

    Add grenade upgrade job
    Change-Id: Iaf3eff58bd4ef66013ea6369add3d241cf8a4fd2

--- devstack/upgrade/resources.sh
+++ devstack/upgrade/resources.sh
@@ -17,27 +17,51 @@ function create_resources {
     local SUBNET2
     local PORT1
     local PORT2
+    local SG1
+    local SG2
+    local SGR11
+    local SGR12
+    local SGR21
+    local SGR22
 
-    NETPART_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 32 | head -n 1)
-    NETPART1=$(neutron  --os-cloud devstack-admin nuage-netpartition-create ${NETPART_NAME} -c id -f value)
+    NETPART_NAME=grenade_$(openssl rand -hex 3)
+    NETPART1=$(openstack --os-cloud devstack-admin nuage netpartition create ${NETPART_NAME} -c id -f value)
     resource_save ${PROJECT} netpartition_nuage ${NETPART1}
 
-    NET1=$(openstack  --os-cloud devstack-admin network create -f value -c id net-nuage)
+    SG1=$(openstack --os-cloud devstack-admin security group create --stateful sg1 -c id -f value)
+    resource_save ${PROJECT} sg1 ${SG1}
+
+    SGR11=$(openstack --os-cloud devstack-admin security group rule create --ingress --protocol tcp --ethertype ipv4 sg1 -c id -f value)
+    resource_save ${PROJECT} sgr11 ${SGR11}
+
+    SGR12=$(openstack --os-cloud devstack-admin security group rule create --ingress --protocol icmp --ethertype ipv4 sg1 -c id -f value)
+    resource_save ${PROJECT} sgr12 ${SGR12}
+
+    SG2=$(openstack --os-cloud devstack-admin security group create --no-stateful sg2 -c id -f value)
+    resource_save ${PROJECT} sg2 ${SG2}
+
+    SGR21=$(openstack --os-cloud devstack-admin security group rule create --ingress --protocol tcp --ethertype ipv4 sg2 -c id -f value)
+    resource_save ${PROJECT} sgr21 ${SGR21}
+
+    SGR22=$(openstack --os-cloud devstack-admin security group rule create --ingress --protocol icmp --ethertype ipv4 sg2 -c id -f value)
+    resource_save ${PROJECT} sgr22 ${SGR22}
+
+    NET1=$(openstack --os-cloud devstack-admin network create -f value -c id net-nuage)
     resource_save ${PROJECT} net_nuage ${NET1}
 
-    SUBNET1=$(openstack  --os-cloud devstack-admin subnet create -f value -c id --ip-version 4 --subnet-range 192.2.0.0/24 --network ${NET1} subnet1)
+    SUBNET1=$(openstack --os-cloud devstack-admin subnet create -f value -c id --ip-version 4 --subnet-range 192.2.0.0/24 --network ${NET1} subnet1)
     resource_save ${PROJECT} subnet1 ${SUBNET1}
 
-    PORT1=$(neutron  --os-cloud devstack-admin port-create -f value -c id --name port1 ${NET1})
+    PORT1=$(openstack --os-cloud devstack-admin port create -f value -c id --network ${NET1} --security-group sg1 port1)
     resource_save ${PROJECT} port1 ${PORT1}
 
-    NET2=$(openstack  --os-cloud devstack-admin network create -f value -c id net_netpart)
+    NET2=$(openstack --os-cloud devstack-admin network create -f value -c id net_netpart)
     resource_save ${PROJECT} net_netpart ${NET2}
 
-    SUBNET2=$(neutron  --os-cloud devstack-admin subnet-create -f value -c id --ip-version 4 --name subnet2 ${NET2} 192.3.0.0/24 --net-partition ${NETPART1})
+    SUBNET2=$(openstack --os-cloud devstack-admin subnet create -f value -c id --ip-version 4 --subnet-range 192.3.0.0/24 --network ${NET2} --net-partition ${NETPART1} subnet2)
     resource_save ${PROJECT} subnet2 ${SUBNET2}
 
-    PORT2=$(neutron  --os-cloud devstack-admin port-create -f value -c id --name port2 ${NET2})
+    PORT2=$(openstack --os-cloud devstack-admin port create -f value -c id --network ${NET2} --security-group sg2 port2)
     resource_save ${PROJECT} port2 ${PORT2}
 }
 
@@ -49,6 +73,8 @@ function verify_resources {
     local NET2
     local PORT1
     local PORT2
+    local SG1
+    local SG2
 
     NETPART=$(resource_get ${PROJECT} netpartition_nuage)
     netpart_name=$(neutron  --os-cloud devstack-admin nuage-netpartition-show $NETPART -c name -f value)
@@ -59,11 +85,19 @@ function verify_resources {
 
 
     NET2=$(resource_get ${PROJECT} net_netpart)
-    net_type=$(openstack  --os-cloud devstack-admin network show -f value -c provider:network_type ${NET2})
+    net_type=$(openstack --os-cloud devstack-admin network show -f value -c provider:network_type ${NET2})
     test "${net_type}" = vxlan
 
     PORT1=$(resource_get ${PROJECT} port1)
     PORT2=$(resource_get ${PROJECT} port2)
+
+    SG1=$(resource_get ${PROJECT} sg1)
+    stateless=$(openstack --os-cloud devstack-admin security group show -c stateful -f value ${SG1})
+    test "${stateless}" = True
+
+    SG2=$(resource_get ${PROJECT} sg2)
+    stateless=$(openstack --os-cloud devstack-admin security group show -c stateful -f value ${SG2})
+    test "${stateless}" = False
 }
 
 function destroy {
@@ -73,21 +107,29 @@ function destroy {
     local NET2
     local PORT1
     local PORT2
+    local SG1
+    local SG2
 
     PORT1=$(resource_get ${PROJECT} port1)
-    openstack  --os-cloud devstack-admin port delete $PORT1
+    openstack --os-cloud devstack-admin port delete $PORT1
 
     PORT2=$(resource_get ${PROJECT} port2)
-    openstack  --os-cloud devstack-admin port delete $PORT2
+    openstack --os-cloud devstack-admin port delete $PORT2
 
     NET1=$(resource_get ${PROJECT} net_nuage)
-    openstack  --os-cloud devstack-admin network delete $NET1
+    openstack --os-cloud devstack-admin network delete $NET1
 
     NET2=$(resource_get ${PROJECT} net_netpart)
-    openstack  --os-cloud devstack-admin network delete $NET2
+    openstack --os-cloud devstack-admin network delete $NET2
+
+    SG1=$(resource_get ${PROJECT} sg1)
+    openstack --os-cloud devstack-admin security group delete $SG1
+
+    SG2=$(resource_get ${PROJECT} sg2)
+    openstack --os-cloud devstack-admin security group delete $SG2
 
     NETPART=$(resource_get ${PROJECT} netpartition_nuage)
-    neutron  --os-cloud devstack-admin nuage-netpartition-delete $NETPART
+    openstack --os-cloud devstack-admin nuage netpartition delete $NETPART
 }
 
 
--- devstack/upgrade/upgrade.sh
+++ devstack/upgrade/upgrade.sh
@@ -1,22 +1,41 @@
-#! /usr/bin/env bash
+echo "*********************************************************************"
+echo "Begin $0"
+echo "*********************************************************************"
+
+set -o xtrace
+
+# gridinv: We prepare venv for further projects upgrade.
+# devstack @ussuri will attempt to do this via python3 -mvenv
+# which fails on centos7 with python3.6.
+# This is needed only on migration T -> U
+install_infra_venv() {
+    local PIP_VIRTUAL_ENV="$REQUIREMENTS_DIR/.venv"
+    virtualenv $PIP_VIRTUAL_ENV
+    $PIP_VIRTUAL_ENV/bin/pip install -U pbr
+    $PIP_VIRTUAL_ENV/bin/pip install $REQUIREMENTS_DIR
+    unset PIP_VIRTUAL_ENV
+}
+
+# Set for DevStack compatibility
 
 source $GRENADE_DIR/grenaderc
 source $GRENADE_DIR/functions
 source $TARGET_DEVSTACK_DIR/stackrc
 
-set -o errexit
-set -x
-
-TOP_DIR=$TARGET_DEVSTACK_DIR
+# Get functions from current DevStack
+source $TARGET_DEVSTACK_DIR/inc/python
 
 # install plugin
 setup_develop $TARGET_RELEASE_DIR/nuage-openstack-neutron
 
+# setup infra venv so that further upgrade does not fail
+install_infra_venv
+
+# gridinv: hack to get upgrade working for U on centos7
+sed -i 's/python3-mod_wsgi/mod_wsgi/g' $TARGET_RELEASE_DIR/devstack/lib/apache
 
 set +x
 set +o xtrace
 echo "*********************************************************************"
 echo "SUCCESS: End $0"
 echo "*********************************************************************"
-
-
