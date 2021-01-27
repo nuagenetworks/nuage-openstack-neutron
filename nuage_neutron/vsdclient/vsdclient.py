573d0432df33759644d26b2d6552ca56e4ffd33b
Vlad Gridin <vladyslav.gridin@nokia.com>
2021-01-27 09:06:54 +0100

    Implement rollback/retry for concurrent port ops
    Improve handling of concurrent port operations in hwvtep
    mechanism driver. This patchset adds rollback on failures
    in port update and retry on DB failures.
    
    Change-Id: I842f26989849772b7c51286526e1a41441c3c3bb

--- nuage_neutron/vsdclient/vsdclient.py
+++ nuage_neutron/vsdclient/vsdclient.py
@@ -439,7 +439,8 @@ class VsdClient(object):
     def create_gateway_vport(self, tenant_id, vport_dict):
         pass
 
-    def create_gateway_vport_no_usergroup(self, tenant_id, vport_dict):
+    def create_gateway_vport_no_usergroup(self, tenant_id, vport_dict,
+                                          on_rollback=None):
         pass
 
     def delete_nuage_gateway_vport(self, context, id, def_netpart_id):
